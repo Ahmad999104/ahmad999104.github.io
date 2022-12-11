e4a882e3a1c92df16bd407923d89950d8e2e6457
Ahmad999104 <79418365+Ahmad999104@users.noreply.github.com>
2022-12-11 09:33:37 +0200

    Add files via upload

---
+++ liberty_insurasce/image.js
@@ -0,0 +1,22 @@
+import React from "react";
+import img1 form './images /creditcards.jpg';
+
+
+
+
+
+
+
+
+function Imagess(){
+
+    return(
+
+        <div>
+          <img src={img1} alt=""/>
+          </div>
+    )
+
+}
+
+ecport default Imagess;
